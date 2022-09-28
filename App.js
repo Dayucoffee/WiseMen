30def44b3667d682129f30e4ef637cc1804ec6dd
Jules! <69649546+Dayucoffee@users.noreply.github.com>
2022-09-28 19:11:15 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import React, { useEffect } from 'react' 
+
+function App() {
+	const [data, setData] = useState([{}])
+
+	useEffect(() => {
+		fetch("/members").then(
+			res => res.json()
+		).then(
+			data => {
+				setData(data)
+				console.log(data)
+			}
+		)
+	}, [])
+
+	return (
+		<div>
+ {(typeof data.members === 'undefined') ? (
+	<p>Loading...</p>
+ ) : (
+	data.members.map((member, i) => (
+		<p key={i}>{member}</p>
+	))
+ )}
+		</div>
+	)
+}
+
+export default App
