50b4d4cc5cf546393d52807525cbb54caea24549
Jules! <69649546+Dayucoffee@users.noreply.github.com>
2022-09-28 19:12:38 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -1,12 +1,25 @@
+# Import flask and datetime module for showing date and time 
 from flask import Flask
+import datetime
 
+x =  datetime.datetime.now()
+
+# Initialize flask app
 app = Flask(__name__)
 
-# Members API Route
+# Route for seeing data
+@app.route('/data')
+def get_time():
 
-@app.route("/members")
-def numbers():
-	return {"members": ["Oscar", "Wilde", "Socrates", "Pluto", "Joker", "Jules"]}
-
-if __name__ == "__main__":
+	 # Returning an api for showing in  reactjs
+    return {
+        'Name':"wilde", 
+        "Age":"84",
+		"Date":x, 
+		"System":"iMac",
+        "Programming":"python"
+        }
+  
+		# Running app
+if __name__ == '__main__':
 	app.run(debug=True)
